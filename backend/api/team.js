d8e41e6843548118f3cf6f3bfc8e36d75c5dd5a6
anirudh <anirudhmurthy2002@gmail.com>
2021-10-18 12:00:14 +0530

    team.js

--- backend/api/team.js
+++ backend/api/team.js
@@ -1,13 +1,24 @@
+const { Router } = require("express");
 const express = require("express")
 const router = express.Router();
 
 // route to fetch board members' details
 router.get("/board", (req, res) => {
+
+    router.find((function(err,info){
+        if(err){
+            console.log(err);
+        }
+        else
+        res.json(info);
+    }))
     
 })
 
 // route to add board members' details
 router.post("/board", (req, res) => {
+
+    
     
 })
 
