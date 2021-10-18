be9cdfb509db433bbca4bdd8e6c22e112ae76767
anirudh <anirudhmurthy2002@gmail.com>
2021-10-18 12:18:07 +0530

    modify team.js

--- backend/api/fame.js
+++ backend/api/fame.js
@@ -9,6 +9,16 @@ router.get("/alumni/:year", (req, res) => {
 // route to add hall of fame members' details
 router.post("/alumni", (req, res) => {
 
+    let info = new Fame(req.body);
+    info.save()
+    .then( info => {
+        res.status(200).json({'info':'info added'});
+    }    )
+
+    .catch(err =>{
+        res.status(400).send('adding fail');
+    });
+
 })
 
 module.exports = router
--- backend/api/team.js
+++ backend/api/team.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
-const express = require("express")
+const express = require("express");
+const Team = require("../schema/Team");
 const router = express.Router();
 
 // route to fetch board members' details
 router.get("/board", (req, res) => {
 
-    router.find((function(err,info){
+    Team.find((function(err,info){
         if(err){
             console.log(err);
         }
@@ -18,8 +19,16 @@ router.get("/board", (req, res) => {
 // route to add board members' details
 router.post("/board", (req, res) => {
 
+    let info = new Team(req.body);
+    info.save()
+    .then( info => {
+        res.status(200).json({'info':'info added'});
+    }    )
+
+    .catch(err =>{
+        res.status(400).send('adding fail');
+    });
     
-    
-})
+});
 
 module.exports = router
