0acad4ef1be62cbef36648f855e85726748f0052
anirudh <anirudhmurthy2002@gmail.com>
2021-10-24 14:27:15 +0530

    added with routes

--- backend/api/fame.js
+++ backend/api/fame.js
@@ -1,13 +1,49 @@
-const express = require("express")
+const express = require("express");
+const Fame = require("../schema/Fame");
 const router = express.Router();
 
 // route to fetch hall of fame members' details
 router.get("/alumni/:year", (req, res) => {
+
+    try {
+        let reso = await Fame.find({
+            category: req.params.year
+        });
+
+        if(!reso) {
+            res.status(401).send({ success: false, message: "Invalid category" });
+        }
+
+        res.status(200).send({ success: true, data: reso });
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Server error" })
+    }
+
     
 })
 
 // route to add hall of fame members' details
 router.post("/alumni", (req, res) => {
+
+
+    try {
+        let fame = new Fame({
+            startup: req.body.startup,
+            logo: req.body.logo,
+            founderName: req.body.founderName,
+            position: req.body.position,
+            photo: req.body.photo,
+            social: req.body.social,
+        });
+        fame.save().then(() => {
+            res.status(200).send({ success: true, message: "Added new resource" });
+        }).catch((err) => {
+            console.log(err);
+            res.status(400).send({ success: false, message: "Error fetching resource" });
+        })  
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Server error" })
+    }
 
 })
 
--- backend/api/team.js
+++ backend/api/team.js
@@ -1,13 +1,48 @@
-const express = require("express")
+const express = require("express");
+const Team = require("../schema/Team");
 const router = express.Router();
 
 // route to fetch board members' details
 router.get("/board", (req, res) => {
+
+    try {
+        let reso = await Team.find({
+            category: req.params
+        });
+
+        if(!reso) {
+            res.status(401).send({ success: false, message: "Invalid category" });
+        }
+
+        res.status(200).send({ success: true, data: reso });
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Server error" })
+    }
+
+    
     
 })
 
 // route to add board members' details
 router.post("/board", (req, res) => {
+
+    try {
+        let team = new Team({
+            category: req.body.name,
+            position: req.body.position,
+            photo: req.body.photo,
+            social: req.body.social,
+            
+        });
+        team.save().then(() => {
+            res.status(200).send({ success: true, message: "Added new resource" });
+        }).catch((err) => {
+            console.log(err);
+            res.status(400).send({ success: false, message: "Error fetching resource" });
+        })  
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Server error" })
+    }
     
 })
 
